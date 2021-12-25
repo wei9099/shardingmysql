f026dfe664ade1767e4a075ce7fb7e6ed7570f05
weiyx <itw1994@126.com>
2021-12-25 15:18:10 +0800

    init

---
+++ src/main/java/com/weiyx/shardingmysql/ShardingmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.weiyx.shardingmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingmysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weiyx/shardingmysql/ShardingmysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiyx.shardingmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingmysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
